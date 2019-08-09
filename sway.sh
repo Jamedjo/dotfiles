df79eaa768ca93f9d713ce1e07a7e8a2afb5afdc
James Edwards-Jones <jedwardsjones@gitlab.com>
2019-08-09 13:21:18 +0100

    GTK persists settings and cursor

--- sway.sh
+++ sway.sh
@@ -1,3 +1,6 @@
 export XDG_CURRENT_DESKTOP=GNOME
 export QT_STYLE_OVERRIDE=adwaita
+export XCURSOR_SIZE=48
+export XCURSOR_THEME=whiteglass
+export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules/
 WLC_DRM_DEVICE=card0 dbus-launch --exit-with-session sway -d 2>sway.log
