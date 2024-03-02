c0f42af25a8f302cf0fed86f9ea394bd5cf333dc
James Edwards-Jones <git@jamedjo.co.uk>
2024-03-02 13:49:46 +0000

    stash

--- Scripts/colourgrid.py
+++ Scripts/colourgrid.py
@@ -6,8 +6,8 @@ for i in range(10 if normalt else 2):
     for j in range(30, 38):
         for k in range(40, 48):
             if normalt:
-                print ("%d;%d;%d: \33[%d;%d;%dm Hello, World! \33[m " %
+                print("%d;%d;%d: \33[%d;%d;%dm Hello, World! \33[m " %
                     (i, j, k, i, j, k, ))
             else:
-                print "\33[%d;%d;%dm%d;%d;%d\33[m " % (i, j, k, i, j, k),
-        print 
+                print("\33[%d;%d;%dm%d;%d;%d\33[m " % (i, j, k, i, j, k)),
+        print() 
