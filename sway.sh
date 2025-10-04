d84e83b67b63f8416fe79c52d079db6a866a6b14
James Edwards-Jones <git@jamedjo.co.uk>
2025-10-04 10:36:48 +0100

    Sway --next-gpu-wont-be-nvidia became --unsupported-gpu

--- sway.sh
+++ sway.sh
@@ -10,4 +10,10 @@ export XCURSOR_THEME=whiteglass
 export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules/
 export LIBSEAT_BACKEND=logind
 unset DISPLAY
-WLC_DRM_DEVICE=card0 WLR_DRM_NO_MODIFIERS=1 dbus-launch --exit-with-session sway -d 2>~/sway.log
+#WLC_DRM_DEVICE=card0 WLR_DRM_NO_MODIFIERS=1 dbus-launch --sh-syntax --exit-with-session sway 2>~/sway.log
+#NOTE: This should be run without dbus-launch when running from a launcher such a ly
+#      Otherwise dbus-launch will be double-ran and DBUS_SESSION_BUS_ADDRESS will be set to a temp file instead of user sessions
+#      This will result in snap packages being unable to be ran as cgroups cannot be created for them
+#WLC_DRM_DEVICE=card0 WLR_DRM_NO_MODIFIERS=1 sway 2>~/sway.log
+#WLC_DRM_DEVICE=card0 WLR_DRM_NO_MODIFIERS=1 sway --my-next-gpu-wont-be-nvidia 2>~/sway.log
+WLC_DRM_DEVICE=card0 WLR_DRM_NO_MODIFIERS=1 sway --unsupported-gpu 2>~/sway.log
